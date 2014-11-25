c3507fc9ce4250f7dd0ea59b27959dc89832e715
Sergey Chikuyonok <serge.che@gmail.com>
2014-11-26 01:27:40 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var streamify = require('gulp-streamify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var through = require('through2');
+
+function cleanup() {
+	return through.obj(function(chunk, enc, next) {
+		var str = chunk.toString();
+		if (str.indexOf(__dirname)) {
+			chunk = new Buffer(str.replace(__dirname, ''));
+		}
+		this.push(chunk);
+		next();
+	});
+}
+
+gulp.task('worker', function() {
+	return browserify({
+		entries: './node_modules/livestyle-patcher/lib/worker.js',
+		detectGlobals: false
+	})
+	.bundle()
+	.pipe(cleanup())
+	.pipe(source('worker.js'))
+	.pipe(gulp.dest('./out'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['./node_modules/livestyle-patcher/lib/*.js'], ['worker']);
+});
+
+gulp.task('default', ['worker']);
