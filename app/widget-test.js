23670e3babc08e28e80e6f3271066f7be75d31e7
Sergey Chikuyonok <serge.che@gmail.com>
2015-02-05 02:03:32 +0300

    Working on widget overlay module

--- app/widget-test.js
+++ app/widget-test.js
@@ -11,6 +11,16 @@ var lineWidget = document.createElement('div');
 lineWidget.className = 'line-widget';
 lineWidget.innerText = 'ul.nav li';
 
-editor.addLineWidget(6, lineWidget, {
-	above: true
+editor.on('change', function(instance, change) {
+	var start = change.from;
+	var delta = {
+		line: change.text.length - change.removed.length, 
+		ch: change.text[0].length - change.removed[0].length
+	};
+
+	console.log(delta, change);
 });
+
+// editor.addLineWidget(6, lineWidget, {
+// 	above: true
+// });
