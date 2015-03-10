f500a931428b5042b8759316b374a7d843682488
Sergey Chikuyonok <serge.che@gmail.com>
2015-03-11 02:43:07 +0300

    Minor tweak in build file

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var jsBundler = require('js-bundler');
 
 gulp.task('js', function() {
 	return gulp.src('./lib/*.js')
-		.pipe(jsBundler({standalone: true, sourceMap: true}))
+		.pipe(jsBundler({standalone: true, sourceMap: false}))
 		.pipe(gulp.dest('./out'));
 });
 
 gulp.task('watch', function() {
-	jsBundler.watch()
+	jsBundler.watch({uglify: false, sourceMap: true});
 	gulp.watch(['./lib/**/*.js', './node_modules/livestyle-patcher/lib/*.js'], ['default']);
 });
 
